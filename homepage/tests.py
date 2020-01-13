25a175482c717423c6681e392229bffb74948c2c
Florent <grenaille.florent85@gmail.com>
2020-01-13 16:53:37 +0100

    Add urls.py and test.py file and configure urls in the settings project

--- homepage/tests.py
+++ homepage/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .views import homepage
 
-# Create your tests here.
+
+class UrlsTestCase(TestCase):
+    def setUp(self):
+        self.cli = Client()
+
+    def test_homepage_status(self):
+        rep = self.cli.get('')
+        self.assertEqual(rep.status_code, 200)
+        self.assertEqual(rep.resolver_match.func, homepage)
+        self.assertTemplateUsed(rep, 'homepage/index.html')
